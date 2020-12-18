5d1023a300f70f42260e7245681ca633a78e7fba
Shiv Jyoti <shivjyoti.garai@gmail.com>
2020-12-19 00:31:41 +0530

    Youtube video fixed and slidingfeature enabled

---
+++ app/src/main/java/com/example/oteshackathon/Config.java
@@ -0,0 +1,9 @@
+package com.example.oteshackathon;
+
+public class Config {
+
+    private Config() {
+    }
+
+    public static final String YOUTUBE_API_KEY = "AIzaSyB74wEd6PUMQeYrN0iCxYO-tpQ5R-z2Tp4";
+}
--- app/src/main/java/com/example/oteshackathon/onboarding_3.java
+++ app/src/main/java/com/example/oteshackathon/onboarding_3.java
@@ -1,19 +1,22 @@
 package com.example.oteshackathon;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class onboarding_3 extends AppCompatActivity {
+
+
+    float x1,x2,y1,y2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_onboarding_3);
-
         Button b1 = findViewById(R.id.button5);
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,4 +27,32 @@ public class onboarding_3 extends AppCompatActivity {
             }
         });
     }
+
+
+    public boolean onTouchEvent(MotionEvent touchEvent){
+        switch(touchEvent.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                x1 = touchEvent.getX();
+                y1 = touchEvent.getY();
+                break;
+            case MotionEvent.ACTION_UP:
+                x2 = touchEvent.getX();
+                y2 = touchEvent.getY();
+                if(x1 <x2){//right swipe
+                    Intent i= new Intent(onboarding_3.this,onboarding_2.class);
+                    startActivity(i);
+                    overridePendingTransition(R.anim.slide_in_left,R.anim.slide_out_right);
+
+                }else if(x1 > x2){//left swipe
+
+
+                    Intent i= new Intent(onboarding_3.this,onboarding_4.class);
+                    startActivity(i);
+                    overridePendingTransition(R.anim.slide_in_right,R.anim.slide_out_left);
+                }
+                break;
+        }
+        return false;
+    }
 }
+
