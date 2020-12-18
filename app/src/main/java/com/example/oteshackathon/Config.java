61360d79ef9e391bae08c76a85ccad11d72725c6
Shiv Jyoti <shivjyoti.garai@gmail.com>
2020-12-18 17:23:07 +0530

    Replaced link with actual YouTube video

---
+++ app/src/main/java/com/example/oteshackathon/Config.java
@@ -0,0 +1,10 @@
+package com.example.oteshackathon;
+
+public final class Config {
+
+    private Config() {
+    }
+
+    public static final String YOUTUBE_API_KEY = "AIzaSyB74wEd6PUMQeYrN0iCxYO-tpQ5R-z2Tp4";
+
+}
